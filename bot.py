5a0e62d5f93e0c1a3a13f921b24ca0670c7fecd2
Your Name <you@example.com>
2023-11-09 11:11:57 +0700

    chgange

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     message = update.message.text
     reply_to_message = update.message.reply_to_message
 
-    if "chốt mua" in message.lower():
-        amount = float(message.split()[2].split("/")[0].replace("u",""))
+    if "chốt mua" in message.lower() or "chốt bán" in message.lower():
+        amount = float(message.split()[2].split("/")[0].replace("usdt","").replace(",",""))
         format_amount = f'{amount:,}'
 
         rate = int(message.split()[2].split("/")[1])
@@ -33,23 +33,30 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         money = amount * rate
         format_money = f'{round(money):,}'
 
+        type_transfer = 'sell'
+        title = 'ĐƠN BÁN USDT - HEROTEAM'
+        if "chốt mua" in message.lower():
+            type_transfer = 'buy'
+            title = 'ĐƠN MUA USDT - HEROTEAM'
+
         data = {
             "seller": username,
             "client": update.effective_chat.title,
             "amount": amount,
             "rate": rate,
             "total_money": money,
-            "type": "buy"
+            "type": type_transfer,
+            "debt": money
         }
 
         res = requests.post(f"{domain}/api/sales", data)
         code = res.json()['data']['code']
 
-        text = f"<b>ĐƠN MUA USDT - HEROTEAM</b>\n<b>{'Mã giao dịch:': <20}</b>{code}\n<b>{'Số lượng:': <24}</b>{format_amount}\n<b>{'Tỷ giá:': <28}</b>{format_rate}<b>\n{'Tổng tiền:': <24}</b>{format_money}"
+        text = f"<b>{title}</b>\n<b>{'Mã giao dịch:': <20}</b>{code}\n<b>{'Số lượng:': <24}</b>{format_amount}\n<b>{'Tỷ giá:': <28}</b>{format_rate}<b>\n{'Tổng tiền:': <24}</b>{format_money}"
         await context.bot.send_message(chat_id=chat_id, text=text, parse_mode=constants.ParseMode.HTML)
     
     if reply_to_message:
-        if "ĐƠN MUA USDT" in reply_to_message.text:
+        if "ĐƠN MUA USDT" in reply_to_message.text or "ĐƠN BÁN USDT" in reply_to_message.text:
             code = reply_to_message.text.split("\n")[1].split()[3]
             res = requests.get(f"{domain}/api/sales/{code}")
             data = res.json()      
@@ -57,7 +64,11 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             paid_usdt = 0
             paid_money = []
             banks = []
-            
+            title = 'ĐƠN BÁN USDT - HEROTEAM'
+
+            if data['type'] == 'buy':
+                title = 'ĐƠN MUA USDT - HEROTEAM'
+
             if data['paid_usdt']:
                 paid_usdt = data['paid_usdt']
 
@@ -68,21 +79,42 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
                 banks = literal_eval(data['bank_name'])
 
 
+            if 'nhập lại' in message.lower():
+                requests.put(f"{domain}/api/sales/{code}",{"paid_money": '', 'bank_name': '', "paid_usdt": 0, "debt": data['total_money']})
+
+                bill = f"<b>{title}</b>\n<b>{'Mã giao dịch:': <20}</b>{code}\n<b>{'Số lượng:': <24}</b>{data['amount']:,}\n<b>{'Tỷ giá:': <28}</b>{data['rate']:,}<b>\n{'Tổng tiền:': <24}</b>{data['total_money']:,}"
+                text = f"{bill}\n————————————————\n<b>👉 VND:\nĐã thanh toán:</b> 0\n<b>Chênh lệch:</b> {data['total_money']:,}"
+                text += f"\n————————————————\n<b>👉 USDT:\nĐã thanh toán:</b> 0\n<b>Chênh lệch:</b> {data['amount']:,}"
+
+                await context.bot.delete_message(message_id=reply_to_message.message_id, chat_id=chat_id)
+                await context.bot.delete_message(message_id=update.message.message_id, chat_id=chat_id)
+                await context.bot.send_message(chat_id=chat_id, text=text, parse_mode=constants.ParseMode.HTML)
+                return
+
+            if 'xóa' in message.lower():
+                requests.delete(f"{domain}/api/sales/{code}")
+                await context.bot.delete_message(message_id=reply_to_message.message_id, chat_id=chat_id)
+                await context.bot.delete_message(message_id=update.message.message_id, chat_id=chat_id)
+                return
+            
             if 'usdt' in message.lower():
                 for i in message.split("\n"):
-                    paid_usdt += float(i.replace('usdt','').replace(",","").replace(".",""))
+                    paid_usdt += float(i.replace('usdt','').replace(",",""))
 
                 requests.put(f"{domain}/api/sales/{code}",{"paid_usdt": paid_usdt})
             
             else:
-                for i in message.split("\n"):
-                    money = i.split("/")[0]
-                    bank = i.split("/")[1]
-                    paid_money.append(int(money.replace(",","").replace(".","")))
-                    banks.append(bank)
+                try:
+                    for i in message.split("\n"):
+                        money = i.split("/")[0]
+                        bank = i.split("/")[1]
+                        paid_money.append(int(money.replace(",","")))
+                        banks.append(bank)
 
-                requests.put(f"{domain}/api/sales/{code}",{"paid_money": json.dumps(paid_money), 'bank_name': json.dumps(banks)})
-
+                    requests.put(f"{domain}/api/sales/{code}",{"paid_money": json.dumps(paid_money), 'bank_name': json.dumps(banks)})
+                except:
+                    await context.bot.delete_message(message_id=update.message.message_id, chat_id=chat_id)
+                    return
 
             paid_text = ''
             paid_total = 0
@@ -94,11 +126,15 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
                 paid_total += i
             if len(paid_money)>1:
                 paid_text += f' = {paid_total:,}'
+            if len(paid_money) == 0:
+                paid_text = 0
 
             money_debt = data['total_money'] - paid_total
             usdt_debt = data['amount'] - paid_usdt
 
-            bill = f"<b>ĐƠN MUA USDT - HEROTEAM</b>\n<b>{'Mã giao dịch:': <20}</b>{code}\n<b>{'Số lượng:': <24}</b>{data['amount']:,}\n<b>{'Tỷ giá:': <28}</b>{data['rate']:,}<b>\n{'Tổng tiền:': <24}</b>{data['total_money']:,}"
+            requests.put(f"{domain}/api/sales/{code}",{"debt": money_debt})
+
+            bill = f"<b>{title}</b>\n<b>{'Mã giao dịch:': <20}</b>{code}\n<b>{'Số lượng:': <24}</b>{data['amount']:,}\n<b>{'Tỷ giá:': <28}</b>{data['rate']:,}<b>\n{'Tổng tiền:': <24}</b>{data['total_money']:,}"
             text = f"{bill}\n————————————————\n<b>👉 VND:\nĐã thanh toán:</b> {paid_text}\n<b>Chênh lệch:</b> {money_debt:,}"
             text += f"\n————————————————\n<b>👉 USDT:\nĐã thanh toán:</b> {paid_usdt:,}\n<b>Chênh lệch:</b> {usdt_debt:,}"
 
@@ -107,7 +143,7 @@ async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             await context.bot.send_message(chat_id=chat_id, text=text, parse_mode=constants.ParseMode.HTML)
 
             if money_debt == 0 and usdt_debt == 0:
-                msg = 'Giao dịch thành công. HeroTeam chân thành cảm ơn quý khách hàng!'
+                msg = 'Giao dịch thành công. <b>HeroTeam</b> chân thành cảm ơn quý khách hàng!'
                 await context.bot.send_message(chat_id=chat_id, text=msg, parse_mode=constants.ParseMode.HTML)
 
 
